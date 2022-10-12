31ced36866eb691832f68db517040098c9e1702c
Ume Habiba <habibaume571@gmail.com>
2022-10-12 01:19:14 -0700

    "local storage added"

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import './App.css';
 import Task from './Task';
-import './newtask';
 function App() {
-
 
   const [mytask, setmytask] = useState('');
   const [mynewtask, setmynewtask] = useState([]);
-
+  useEffect(() => {
+   const data =JSON.parse(localStorage.getItem("items"));
+   console.log(data);
+   if(data){
+    setmynewtask(data);
+   }
+  
+  },[]);
   function settask(e) {
     setmytask(e.target.value);
 
   }
+
+
   function submit() {
     const item =
     {
       "id": mynewtask.length + 1,
       "task": mytask,
       "complete": false,
-      "notified" : false
+      "notified": false
     };
     const newTodos = mynewtask.slice();
     newTodos.push(item);
     setmynewtask(newTodos);
+    localStorage.setItem("items", JSON.stringify(newTodos));
     setmytask('');
+
 
   }
 
 
   function remove(del) {
     const filtered = mynewtask.filter(item => {
-      if (item.id !== del )
+      if (item.id !== del)
         return item;
     })
-    setmynewtask( filtered)
+    setmynewtask(filtered)
+    localStorage.setItem("items", JSON.stringify(filtered));
+
   }
 
   function checked(id, com) {
@@ -41,15 +52,17 @@ function App() {
       return items.id === id ? { ...items, complete: !items.complete } : { ...items }
     });
     setmynewtask(check);
+    localStorage.setItem("items", JSON.stringify(check));
+
   }
 
 
-function notify(id, n) {
-  let change = mynewtask.map(items =>{
-    return items.id === id ? {...items , notified :!items.notified} : {...items}
-  });
-  setmynewtask(change);
-}
+  function notify(id, n) {
+    let change = mynewtask.map(items => {
+      return items.id === id ? { ...items, notified: !items.notified } : { ...items }
+    });
+    setmynewtask(change);
+  }
 
 
   return (
